1712e2d2161df26d762fb76b536e714dc1296d2e
Caue-Sampaio <manicraftkaka1@gmail.com>
2026-03-17 16:33:43 -0300

    começando o index

---
+++ Frontend/index.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" integrity="sha512-Tri8GkI0a8zSIz+n9qGJQp6E42R8+p1Xq2kWc+n7SwL3B38UaYkJaPWe+BMbgC7oP5s8N8u7mJ7+0s1x8u8+Kg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://unpkg.com/flowbite@1.6.5/dist/flowbite.min.js"></script>
+  </head>
+  <body class="pt-24">
+   
+
+
+
+
+<nav class="bg-white fixed w-full z-20 top-0 left-0 border-b border-gray-200">
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <a href="#" class="flex items-center space-x-3">
+        <img src="../img/LogoA.png" class="h-20" alt="Flowbite Logo">
+        <span class="self-center text-xl text-slate-900 font-semibold whitespace-nowrap">SmartClinic-A</span>
+    </a>
+    <div class="flex md:order-2 space-x-2 md:space-x-0 rtl:space-x-reverse">
+        <button type="button" class="text-white bg-blue-600 hover:bg-blue-700 border border-transparent focus:ring-4 focus:ring-blue-300 shadow-sm font-medium rounded-md text-sm px-3 py-2 focus:outline-none">Get started</button>
+        <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-slate-700 rounded-md md:hidden hover:bg-slate-100 focus:outline-none focus:ring-2 focus:ring-blue-500" aria-controls="navbar-sticky" aria-expanded="false">
+            <span class="sr-only">Open main menu</span>
+            <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h14"/></svg>
+        </button>
+    </div>
+    <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+      <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-200 rounded-md bg-white md:space-x-8 md:flex-row md:mt-0 md:border-0 md:bg-transparent">
+        <li>
+          <a href="#" class="block py-2 px-3 text-blue-600 md:text-blue-600 md:bg-transparent md:p-0" aria-current="page">Início</a>
+        </li>
+        <li>
+          <a href="#" class="block py-2 px-3 text-slate-700 md:text-slate-700 hover:text-blue-600 md:p-0">About</a>
+        </li>
+        <li>
+          <a href="#" class="block py-2 px-3 text-slate-700 md:text-slate-700 hover:text-blue-600 md:p-0">Services</a>
+        </li>
+        <li>
+          <a href="#" class="block py-2 px-3 text-slate-700 md:text-slate-700 hover:text-blue-600 md:p-0">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+<div class="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs">
+    <a href="#">
+        <img class="rounded-base" src="../img/images.jpg" alt="" />
+    </a>
+    <a href="#">
+        <h5 class="mt-6 mb-2 text-2xl font-semibold tracking-tight text-heading">Streamlining your design process today.</h5>
+    </a>
+    <p class="mb-6 text-body">In today’s fast-paced digital landscape, fostering seamless collaboration among Developers and IT Operations.</p>
+    <a href="#" class="inline-flex items-center text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">
+        Read more
+        <svg class="w-4 h-4 ms-1.5 rtl:rotate-180 -me-0.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 12H5m14 0-4 4m4-4-4-4"/></svg>
+    </a>
+</div>
+
+  </body>
+</html>
